3cb62f9df9c948b58afc594b4c1e3ad5ccdb1cfd
narimenCH <narimen@taupify.com>
2023-02-18 10:29:58 +0100

    project-redux

---
+++ src/index.js
@@ -0,0 +1,21 @@
+
+import React from 'react';
+import ReactDOM from 'react-dom';
+import App from './App';
+
+import { Provider } from 'react-redux';
+import store from './redux/store';
+
+
+import { ChakraProvider } from '@chakra-ui/react';
+
+ReactDOM.render(
+  <Provider store={store}>
+    <ChakraProvider>
+      <React.StrictMode>
+        <App />
+      </React.StrictMode>
+    </ChakraProvider>
+  </Provider>,
+  document.getElementById('root')
+);
